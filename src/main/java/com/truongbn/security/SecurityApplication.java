4b638ed242d3ff96c50cb74c8c7c44f37955e30c
gloeches <gloeches@gmail.com>
2023-09-29 08:53:08 +0200

    configurado para trabajar en los entornos desarrollo y producción

--- src/main/java/com/truongbn/security/SecurityApplication.java
+++ src/main/java/com/truongbn/security/SecurityApplication.java
@@ -2,9 +2,10 @@ package com.truongbn.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
